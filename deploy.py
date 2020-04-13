eb1b6f51cae7c2ca480c341c051e7bf408c67f03
David Simowitz <david.simowitz@gmail.com>
2020-04-13 18:48:54 -0400

    feat: setup scaffolding

---
+++ deploy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+"""
+AWS CloudFormation deploy script
+"""
+
+
+def deploy_stack():
+    """deploy infrastructure as specified in CloudFormation template"""
+    pass
+
+
+def create_stack():
+    """create stack as specified in CloudFormation template"""
+    pass
+
+
+def update_stack():
+    """update stack as specified in CloudFormation template"""
+    pass
+
+
+def delete_stack():
+    """delete specified stack"""
+    pass
