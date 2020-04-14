201f549442743f8f8bf0d795985c59fdbcc0fe5d
David Simowitz <david.simowitz@gmail.com>
2020-04-13 20:13:26 -0400

    feat: create stack

--- deploy.py
+++ deploy.py
@@ -6,14 +6,22 @@ AWS CloudFormation deploy script
 """
 
 
+import os
+
+
 def deploy_stack():
     """deploy infrastructure as specified in CloudFormation template"""
     pass
 
 
-def create_stack():
+def create_stack(name, template, parameters, region):
     """create stack as specified in CloudFormation template"""
-    pass
+    os.system(f'aws cloudformation create-stack'
+                  f' --stack-name {name}'
+                  f' --template-body file://{template}'
+                  f' --parameters file://{parameters}'
+                  f' --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
+                  f' --region={region}')
 
 
 def update_stack():
