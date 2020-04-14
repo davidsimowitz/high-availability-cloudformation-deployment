5292c6f9fd33ba97625781de85a636245888f4a7
David Simowitz <david.simowitz@gmail.com>
2020-04-13 22:04:31 -0400

    style: blacken code
    https://github.com/psf/black

--- deploy.py
+++ deploy.py
@@ -31,40 +31,47 @@ def deploy_stack(config_file, command):
     except (FileNotFoundError, IsADirectoryError, PermissionError) as err:
         click.echo(f"ERROR: {err}")
     else:
-        exec(f'{command}_stack(**stack)')
+        exec(f"{command}_stack(**stack)")
 
 
 def create_stack(name, template, parameters, region):
     """create stack as specified in CloudFormation template"""
-    os.system(f'aws cloudformation create-stack'
-                  f' --stack-name {name}'
-                  f' --template-body file://{template}'
-                  f' --parameters file://{parameters}'
-                  f' --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
-                  f' --region={region}')
+    os.system(
+        f"aws cloudformation create-stack"
+        f" --stack-name {name}"
+        f" --template-body file://{template}"
+        f" --parameters file://{parameters}"
+        f' --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
+        f" --region={region}"
+    )
 
 
 def update_stack(name, template, parameters, region):
     """update stack as specified in CloudFormation template"""
-    os.system(f'aws cloudformation update-stack'
-                  f' --stack-name {name}'
-                  f' --template-body file://{template}'
-                  f' --parameters file://{parameters}'
-                  f' --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
-                  f' --region={region}')
+    os.system(
+        f"aws cloudformation update-stack"
+        f" --stack-name {name}"
+        f" --template-body file://{template}"
+        f" --parameters file://{parameters}"
+        f' --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
+        f" --region={region}"
+    )
 
 
 def delete_stack(name, **kwargs):
     """delete specified stack"""
-    os.system(f'aws cloudformation delete-stack'
-                  f' --stack-name {name}')
+    os.system(f"aws cloudformation delete-stack" f" --stack-name {name}")
 
 
 @click.command()
-@click.argument('config-file')
-@click.option('-c', '--command', required=True, default='update',
-               type=click.Choice(['create', 'update', 'delete'],
-               case_sensitive=False))
+@click.argument("config-file")
+@click.option(
+    "-c",
+    "--command",
+    required=True,
+    default="update",
+    type=click.Choice(["create", "update", "delete"], case_sensitive=False),
+)
 def cli(*, config_file, command):
     """Deploy infrastructure specified in CONFIG_FILE
 
@@ -73,5 +80,5 @@ def cli(*, config_file, command):
     deploy_stack(config_file, command)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
